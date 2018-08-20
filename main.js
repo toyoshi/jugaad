ab9d0ead025bf65443eb99f7e33810b7913e9bb4
toyoshi <toyosystem@gmail.com>
2018-08-20 12:00:16 +0900

    無駄なコードを削除

--- main.js
+++ main.js
@@ -15,23 +15,21 @@ Vue.component('item-component', {
 </article>
     `,
     props: ['id', 'name', 'init_cost', 'performance', 'counter'],
-    data: function() {
+    data: function () {
         return {
-          qty: 0,
-          cost: this.init_cost,
+            qty: 0,
+            cost: this.init_cost,
         }
     },
-    created: function() {
+    created: function () {
         setInterval(this.produce, this.performance[1]);
     },
     watch: {
         qty: {
-            handler: function(newVal, oldVal){
-                if(oldVal == 4 && newVal == 5){
-                    if(this.id != 15){
-                        //次の施設をアンロック
-                        this.$emit('unlock-next-building');
-                    }
+            handler: function (newVal, oldVal) {
+                //5つ作ったら次の施設をアンロック
+                if (oldVal == 4 && newVal == 5) {
+                    this.$emit('unlock-next-building');
                 }
             }
         }
@@ -40,20 +38,18 @@ Vue.component('item-component', {
         addItem: function () {
             //コストを払う
             this.decSushi(this.cost);
-            
             //コストをあげる
             this.cost = Math.floor(this.cost * 1.2);
-            
             //カウントを増やす
             this.qty++;
         },
-        produce: function() {
+        produce: function () {
             this.incSushi(this.qty * this.performance[0]);
         },
-        incSushi: function(sushi_qty){
+        incSushi: function (sushi_qty) {
             this.$emit('change-total-qty', sushi_qty);
         },
-        decSushi: function(sushi_qty){
+        decSushi: function (sushi_qty) {
             this.incSushi(-1 * sushi_qty);
         }
     }
@@ -63,36 +59,37 @@ var app = new Vue({
     el: '#app',
     data: {
         counter: 0,
-        twUrl: '',
         unlocked_items: [
             //{id: 0, name: 'デバッグ', cost: 1, performance: [1,1000] },
-            {id: 1, name: '見習い職人', cost: 15, performance: [1,10000] },
+            { id: 1, name: '見習い職人', cost: 15, performance: [1, 10000] },
         ],
         locked_items: [
-            {id: 2, name: 'かっぱ', cost: 100, performance: [1, 1000] },
-            {id: 3, name: '農場', cost: 1100, performance: [8, 1000] },
-            {id: 4, name: '鉱山', cost: 12000, performance: [47, 1000] },
-            {id: 5, name: '工場', cost: 130000, performance: [260, 1000] },
-            {id: 6, name: '銀行', cost: 1400000, performance: [1400, 1000] },
-            {id: 7, name: '寺院', cost: 20000000, performance: [7800, 1000] },
-            {id: 8, name: '将太', cost: 330000000, performance: [44000, 1000] },
-            {id: 9, name: '宇宙船', cost: 5100000000, performance: [260000, 1000] },
-            {id: 10, name: '錬金術', cost: 75000000000, performance: [1600000, 1000] },
-            {id: 11, name: 'ポータル', cost: 1000000000000, performance: [10000000, 1000] },
-            {id: 12, name: 'タイムマシン', cost: 14000000000000, performance: [65000000, 1000] },
-            {id: 13, name: '反物質凝縮器', cost: 170000000000000, performance: [43000000, 1000] },
-            {id: 14, name: '大人のかっぱ', cost: 2100000000000000, performance: [2900000000, 1000] },
-            {id: 15, name: 'チャンスメーカー', cost: 26000000000000000, performance: [21000000000, 1000] },
+            { id: 2, name: 'かっぱ', cost: 100, performance: [1, 1000] },
+            { id: 3, name: '農場', cost: 1100, performance: [8, 1000] },
+            { id: 4, name: '鉱山', cost: 12000, performance: [47, 1000] },
+            { id: 5, name: '工場', cost: 130000, performance: [260, 1000] },
+            { id: 6, name: '銀行', cost: 1400000, performance: [1400, 1000] },
+            { id: 7, name: '寺院', cost: 20000000, performance: [7800, 1000] },
+            { id: 8, name: '将太', cost: 330000000, performance: [44000, 1000] },
+            { id: 9, name: '宇宙船', cost: 5100000000, performance: [260000, 1000] },
+            { id: 10, name: '錬金術', cost: 75000000000, performance: [1600000, 1000] },
+            { id: 11, name: 'ポータル', cost: 1000000000000, performance: [10000000, 1000] },
+            { id: 12, name: 'タイムマシン', cost: 14000000000000, performance: [65000000, 1000] },
+            { id: 13, name: '反物質凝縮器', cost: 170000000000000, performance: [43000000, 1000] },
+            { id: 14, name: '大人のかっぱ', cost: 2100000000000000, performance: [2900000000, 1000] },
+            { id: 15, name: 'チャンスメーカー', cost: 26000000000000000, performance: [21000000000, 1000] },
         ]
     },
     methods: {
-        unLockNetxBuilding: function(id) {
-            this.unlocked_items.push(this.locked_items.shift(0)); //盲目的に次の施設をアンロックすることにする
+        unLockNetxBuilding: function (id) {
+            if (this.locked_items.length > 0) {
+                this.unlocked_items.push(this.locked_items.shift(0)); //無条件でリストの次の施設をアンロックすることにする
+            }
         },
-        changeTotalQty: function(qty) {
+        changeTotalQty: function (qty) {
             this.counter = this.counter + qty;
         },
-        createSnsUrl : function(){
+        createSnsUrl: function () {
             var url = encodeURIComponent(location.href);
             var txt = encodeURIComponent(this.counter + '貫の寿司を握りました。🍣クリッカー @toyoshi');
             return 'https://twitter.com/intent/tweet?text=' + txt + '&url=' + url;
